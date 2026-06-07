b3363ae1ea8d26305b1c79efb1b734fe78faf6a8
Chrisg <gilbertchristian25@gmail.com>
2026-06-07 12:50:51 +0900

    Boiler plate

---
+++ UAS_202565001.py
@@ -0,0 +1,18 @@
+'''
+Nama: Christian Gilbert Rusianto
+NIM: 202565001
+Kelas: A
+
+Github: https://github.com/Rick-beep
+'''
+
+import json
+
+class Perpus():
+    def __init__(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    pass
